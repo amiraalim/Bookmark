4c171e6d9cd9e146e8e1ba94b057d31c45c66bbb
Amira Alim <amira_ainul@yahoo.com>
2014-04-25 18:25:17 +0100

    new conn

--- includes/conn.php
+++ includes/conn.php
@@ -10,11 +10,22 @@ $username ='mysqluser';
 /*** mysql password ***/
 $password = 'password';
 
+//AWS db variables
 $dbname = 'bookmark';
+$dbhost ='aa1bqvobjxv66a7.cudlsl6p6qkx.us-east-1.rds.amazonaws.com';
+$dbport='3306';
+
+
+
 
 /*** connect to the database ***/
 //$link = @mysql_connect('161.76.79.2:3306', $username, $password);
-$sql = new mysqli($hostname,$username,$password,$dbname);
+
+/***GAE connection***/
+//$sql = new mysqli($hostname,$username,$password,$dbname);
+
+/***AWS connection***/
+$sql = mysqli_connect($dbhost, $username, $password, $dbname, $dbport);	
 
 /*** select the database ***/
 //$db=mysql_select_db('bookmark');
